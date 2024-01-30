2029ce3ebc88dfbcbfc59a3b6afe3f4e9cd89c79
Dominik Noga <dominik.noga@felten-group.com>
2024-01-30 18:21:09 +0100

    Created basic ui components and divided to desired file structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ChallengePageComponent } from './pages/challenge-page/challenge-page.component';
 
-const CHALLENGES_AMOUNT = 100;
-
-const routeNames = Array.from({length: CHALLENGES_AMOUNT}, (value, index) => `challenge-${index}`)
-
-const routes: Routes = routeNames.map(routeName => ({
-  path: routeName
-}));
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent
+  },
+  {
+    path: 'challenge/:id',
+    component: ChallengePageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
