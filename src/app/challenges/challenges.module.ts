be1f45849daa1f2b418b0a7e02d775982d599cea
Dominik Noga <dominik.noga@felten-group.com>
2024-01-31 10:34:51 +0100

    Finished challenge 1

--- src/app/challenges/challenges.module.ts
+++ src/app/challenges/challenges.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { Challenge1Component } from './challenge1/challenge1.component';
 import { Challenge2Component } from './challenge2/challenge2.component';
 import { Challenge3Component } from './challenge3/challenge3.component';
+import { CommonModule } from "@angular/common";
 
 @NgModule({
   declarations: [
@@ -9,7 +10,9 @@ import { Challenge3Component } from './challenge3/challenge3.component';
     Challenge2Component,
     Challenge3Component
   ],
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   exports: [
     Challenge1Component,
     Challenge2Component,
