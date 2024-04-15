6913d63a5b5d54a3e78ae0163471739a5139f040
DominikNoga <dominik.noga2001@gmail.com>
2024-04-15 12:29:57 +0200

    displayed polylines with points on them. Need to refactor a bit and add tooltips

--- src/app/challenges/challenge5/challenge5.component.ts
+++ src/app/challenges/challenge5/challenge5.component.ts
@@ -1,16 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-
+interface Point {
+  x: number;
+  y: number;
+}
 @Component({
   selector: 'app-challenge5',
   templateUrl: './challenge5.component.html',
   styleUrls: ['./challenge5.component.scss']
 })
 export class Challenge5Component implements OnInit {
+
   readonly revenue: number = 3621.79;
+  readonly days = ['mon', 'tue', 'wen', 'thu', 'fri', 'sat', 'sun'];
+  readonly dataRedPoints: Point[] = [
+    { x: 10, y: 46 },
+    { x: 50, y: 12 },
+    { x: 90, y: 23 },
+    { x: 130, y: 11 },
+    { x: 171, y: 38 },
+    { x: 211, y: 48 },
+    { x: 271, y: 19 }
+  ];
+
+  readonly dataBluePoints = [
+    { x: 10, y: 26 },
+    { x: 50, y: 40 },
+    { x: 90, y: 13 },
+    { x: 130, y: 2 },
+    { x: 171, y: 8 },
+    { x: 240, y: 48 },
+    { x: 285, y: 19 }
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  getPolylinePoints(points: Point[]) {
+    return points.map(point => `${point.x}, ${point.y}`).join(" ");
+  }
 }
