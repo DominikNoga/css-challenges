19ce6f7154db1559cf1aa0eeb76ea899078d6923
DominikNoga <dominik.noga2001@gmail.com>
2025-02-05 13:51:13 +0100

    challenge-6

--- src/app/challenges/challenges.module.ts
+++ src/app/challenges/challenges.module.ts
@@ -5,6 +5,7 @@ import { Challenge3Component } from './challenge3/challenge3.component';
 import { CommonModule } from "@angular/common";
 import { Challenge4Component } from './challenge4/challenge4.component';
 import { Challenge5Component } from './challenge5/challenge5.component';
+import { Challenge6Component } from './challenge6/challenge6.component';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { Challenge5Component } from './challenge5/challenge5.component';
     Challenge2Component,
     Challenge3Component,
     Challenge4Component,
-    Challenge5Component
+    Challenge5Component,
+    Challenge6Component
   ],
   imports: [
     CommonModule
--- src/app/ui-components/challenge-renderer/challenge-renderer.constants.ts
+++ src/app/ui-components/challenge-renderer/challenge-renderer.constants.ts
@@ -4,6 +4,7 @@ import { Challenge2Component } from "src/app/challenges/challenge2/challenge2.co
 import { Challenge3Component } from "src/app/challenges/challenge3/challenge3.component";
 import { Challenge4Component } from "src/app/challenges/challenge4/challenge4.component";
 import { Challenge5Component } from "src/app/challenges/challenge5/challenge5.component";
+import { Challenge6Component } from "src/app/challenges/challenge6/challenge6.component";
 
 export const COMPONENT_MAP: { [key: string]: Type<any> } = {
     '1': Challenge1Component,
@@ -11,6 +12,7 @@ export const COMPONENT_MAP: { [key: string]: Type<any> } = {
     '3': Challenge3Component,
     '4': Challenge4Component,
     '5': Challenge5Component,
+    '6': Challenge6Component
 };
 
 export const ALLOWED_NUMBERS = Object.keys(COMPONENT_MAP);
@@ -18,7 +20,8 @@ export const ALLOWED_NUMBERS = Object.keys(COMPONENT_MAP);
 export const FINISHED_ON: { [key: string]: string } = {
     '1': '30.01.2024',
     '2': '31.01.2024',
-    '3': 'in progress',
+    '3': '05.02.2024',
     '4': '12.02.2024',
-    '5': 'in progress',
+    '5': '15.02.2024',
+    '6': '18.02.2024'
 }
